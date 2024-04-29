2c5d6b8b4b4636ab56d31bb54f5de5813d9c1cc9
danison <danison.santos00@gmail.com>
2024-04-29 18:35:40 -0300

    refact: add return into save task repository

--- source/backend/challenge/modules/task/repositories/TaskRepository.java
+++ source/backend/challenge/modules/task/repositories/TaskRepository.java
@@ -1,6 +1,7 @@
 package backend.challenge.modules.task.repositories;
 
 import backend.challenge.modules.task.dtos.TaskDTO;
+import backend.challenge.modules.task.enums.TaskStatus;
 import backend.challenge.modules.task.models.Task;
 import lombok.NoArgsConstructor;
 
@@ -10,12 +11,14 @@ import javax.sql.DataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Singleton
@@ -24,11 +27,10 @@ public class TaskRepository implements ITaskRepository {
 	@Inject
 	private DataSource db;
 
-
 	@Override
-	public Task index(final Long taskId) {
+	public Optional<Task> index(final UUID taskId) {
 		// TODO: Criar método responsável por retornar tarefa por id
-
+		
 		return null;
 	}
 
@@ -45,17 +47,28 @@ public class TaskRepository implements ITaskRepository {
 		try (Connection connection = this.db.getConnection()) {
 			String sql = "INSERT INTO tasks(id, title, description, createdAt) VALUES(?, ?, ?, ?)";
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, UUID.randomUUID().toString());
-			preparedStatement.setString(2, taskDTO.getTitle());
-			preparedStatement.setString(3, taskDTO.getDescription());
-			preparedStatement.setTimestamp(4,  Timestamp.valueOf(LocalDateTime.now()));
+			Task newTask = new Task(
+					UUID.randomUUID(),
+					taskDTO.getTitle(),
+					taskDTO.getDescription(),
+					0,
+					TaskStatus.PROGRESS,
+					LocalDateTime.now()
+
+			);
+
+			preparedStatement.setString(1, newTask.getId().toString());
+			preparedStatement.setString(2, newTask.getTitle());
+			preparedStatement.setString(3, newTask.getDescription());
+			preparedStatement.setTimestamp(4, Timestamp.valueOf(newTask.getCreatedAt()));
 			preparedStatement.executeUpdate();
 
+			return newTask;
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		return null;
+
 	}
 
 	@Override
