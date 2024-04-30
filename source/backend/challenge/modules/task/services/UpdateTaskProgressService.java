1abad911b35dc080b8842ee99f91f8bb0e70e2e8
danison <danison.santos00@gmail.com>
2024-04-30 01:20:24 -0300

    fix: chage status if full progress

--- source/backend/challenge/modules/task/services/UpdateTaskProgressService.java
+++ source/backend/challenge/modules/task/services/UpdateTaskProgressService.java
@@ -4,12 +4,12 @@ import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import backend.challenge.modules.task.dtos.TaskProgressDTO;
+import backend.challenge.modules.task.enums.TaskStatus;
 import backend.challenge.modules.task.exceptions.ProgressTaskUpdateNotAvailable;
 import backend.challenge.modules.task.exceptions.TaskAlterationNotAvailable;
 import backend.challenge.modules.task.exceptions.TaskNotFound;
 import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
-
 
 @Singleton
 public class UpdateTaskProgressService implements IUpdateTaskProgressService {
@@ -22,7 +22,7 @@ public class UpdateTaskProgressService implements IUpdateTaskProgressService {
     }
 
     @Override
-    public Task execute(TaskProgressDTO taskProgressDTO) throws TaskAlterationNotAvailable, TaskNotFound{
+    public Task execute(TaskProgressDTO taskProgressDTO) throws TaskAlterationNotAvailable, TaskNotFound {
 
         if (taskProgressDTO.getProgress() > 100 || taskProgressDTO.getProgress() < 0) {
             throw new ProgressTaskUpdateNotAvailable();
@@ -34,9 +34,14 @@ public class UpdateTaskProgressService implements IUpdateTaskProgressService {
 
         Task task = taskOpt.get();
         task.setProgress(taskProgressDTO.getProgress());
+        if (taskProgressDTO.getProgress() == 100) {
+            task.setStatus(TaskStatus.COMPLETE);
+        } else {
+            task.setStatus(TaskStatus.PROGRESS);
+        }
+
         iTaskRepository.update(task);
         return task;
     }
-
 
 }
